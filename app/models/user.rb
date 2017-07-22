18f4fc2111dfe46192b85e7d7d5fbd868e4407df
Travis Cole <iamtraviscole@gmail.com>
2017-07-21 20:21:21 -0400

    added needed_ingredients method to clean up what_can_i_make

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,34 +15,26 @@ class User < ActiveRecord::Base
       you_can_make: [],
       you_can_almost_make: []
     }
-    # ex. results = {
-    #   you_can_make: [<#Recipe>, <#Recipe2>, <#Recipe3>],
-    #   you_can_almost_make: [
-    #     {recipe: Recipe, needed_ingredients: [<#Ingredient>, <#Ingredient2>, <#Ingredient3>]},
-    #     {recipe: Recipe2, needed_ingredients: [<#Ingredient6>, <#Ingredient8>, <#Ingredient11>]},
-    #   ]
-    # }
 
     if self.ingredients.ids.size >= 1
 
       user_ingredient_ids = self.ingredients.ids.uniq
       all_ingredient_ids = {}
-      # ex. all_ingredient_ids = {
-      #   recipe_id1: [1,2,3,4],
-      #   recipe_id2: [3,4,7]
-      # }
 
       Recipe.all.each do |recipe|
         all_ingredient_ids["#{recipe.id}"] = recipe.ingredients.ids
       end
 
       all_ingredient_ids.each do |recipe_id, ingredient_ids|
-        user_ingredient_ids_intersection = user_ingredient_ids & ingredient_ids #arry of user ingredients that match recipe ingredients
+        user_ingredient_ids_intersection = user_ingredient_ids & ingredient_ids #array of user ingredients that match recipe ingredients
         if user_ingredient_ids_intersection.size >= 1
           if ingredient_ids.size == user_ingredient_ids_intersection.size
-            results[:you_can_make] << Recipe.find_by(id: recipe_id) #adds recipe to you_can_make array in results hash
+            results[:you_can_make] << Recipe.find_by(id: recipe_id)
           elsif ingredient_ids.size - 2 == user_ingredient_ids_intersection.size || ingredient_ids.size - 1 == user_ingredient_ids_intersection.size
-            results[:you_can_almost_make] << {recipe: Recipe.find_by(id: recipe_id), needed_ingredients: (ingredient_ids - user_ingredient_ids_intersection | user_ingredient_ids_intersection - ingredient_ids).map {|ing_id| Ingredient.find_by(id: ing_id)}}  #adds recipe to you_can_almost_make and needed ingredients to results hash
+            results[:you_can_almost_make] << {
+              recipe: Recipe.find_by(id: recipe_id),
+              needed_ingredients: needed_ingredients(ingredient_ids, user_ingredient_ids_intersection)
+              }
           end
         end
       end
@@ -50,6 +42,12 @@ class User < ActiveRecord::Base
     end
 
     results
+  end
+
+  def needed_ingredients(ingredient_ids, user_ingredient_ids)
+    (ingredient_ids - user_ingredient_ids | user_ingredient_ids - ingredient_ids).map do |ing_id|
+      Ingredient.find_by(id: ing_id)
+    end
   end
 
 end
